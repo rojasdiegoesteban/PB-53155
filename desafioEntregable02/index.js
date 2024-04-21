a473fbc7d96238d69743a1323af5aa9fb6700da1
drojas <drojas@cys.com.ar>
2024-04-21 13:59:25 -0300

    Agrego validaciones

--- desafioEntregable02/index.js
+++ desafioEntregable02/index.js
@@ -34,19 +34,28 @@ const addProduct = async (title, description, price, thumbnail, code, stock) => 
   await fs.promises.writeFile(pathFile, JSON.stringify(products));
 };
 
-const getProducts = async () => {
-  // leo el contenido del archivo y lo guardo en formato string
-  const productsJson = await fs.promises.readFile(pathFile, "utf8");
-  //convierto el resultado de tipo texto a json para tener acceso a cada posicion del array
-  products = JSON.parse(productsJson) || [];
+const getProductsJson = async () => {
+  try {
+    // leo el contenido del archivo y lo guardo en formato string
+    const productsJson = await fs.promises.readFile(pathFile, "utf8");
+    //convierto el resultado de tipo texto a json para tener acceso a cada posicion del array
+    products = JSON.parse(productsJson) || [];
+    return products;  
 
-  // console.log(products);
-  return products;
+    //Si no existe el archivo devuelvo un array vacio
+  } catch (error) {
+    return [];
+  }
 };
+
+const getProducts = async () => {
+  await getProductsJson();
+  console.log(products);
+}
 
 const getProductById = async (id) => {
   // cargo el array products con los datos del archivo json
-  await getProducts();
+  await getProductsJson();
   //Busco en el array el producto por id
   const product = products.find((product) => product.id === id);
 
@@ -61,9 +70,12 @@ const getProductById = async (id) => {
 
 // Metodo para actualizar un producto
 const updateProduct = async (id, dataProduct) => {
-  await getProducts();
+  await getProductsJson();
   // Busco la posicion indice dentro del array products
   const index = products.findIndex((product) => product.id === id);
+  if (index === -1) {
+    console.log(`No se encontró el producto con el id ${id} para actualizar`);
+  }
   
   products[index] = {
     // Hago una copia del producto
@@ -77,11 +89,14 @@ const updateProduct = async (id, dataProduct) => {
 
 // Metodo para eliminar un producto
 const deleteProduct = async (id) => {
-  await getProducts();
+  await getProductsJson();
   // products = products.filter( product => product.id !== id);
 
   //Busco el indice del producto que necesito eliminar
   const index = products.findIndex((product) => product.id === id);
+  if (index === -1) {
+    console.log(`No se encontró el producto con el id ${id} para eliminar`);
+  }
   // Elimino el producto en el indice especificado
   products.splice(index, 1);
 
@@ -91,24 +106,38 @@ const deleteProduct = async (id) => {
 
 
 // Test
-addProduct("Camisa de algodón", "Camisa de algodón de manga larga", 29.99, "https://ejemplo.com/camisa.jpg", "CM001", 50);
+// LLamo a getProducts() sin haber cargado ningun producto
+getProducts();
+
+// LLamo a getProductById() sin haber cargado ningun producto
+// getProductById(2);
+
+// Agrego productos
+/* addProduct("Camisa de algodón", "Camisa de algodón de manga larga", 29.99, "https://ejemplo.com/camisa.jpg", "CM001", 50);
 addProduct("Pantalones vaqueros", "Pantalones vaqueros de corte clásico", 39.99, "https://ejemplo.com/pantalones.jpg", "PN002", 30);
 addProduct("Zapatillas deportivas", "Zapatillas deportivas para correr", 49.99, "https://ejemplo.com/zapatillas.jpg", "ZP003", 25);
 addProduct("Chaqueta de cuero", "Chaqueta de cuero genuino", 99.99, "https://ejemplo.com/chaqueta.jpg", "CH004", 20);
-addProduct("Sombrero de paja", "Sombrero de paja trenzada", 19.99, "https://ejemplo.com/sombrero.jpg", "SH005", 40);
 addProduct("Bufanda de lana", "Bufanda de lana suave y cálida", 14.99, "https://ejemplo.com/bufanda.jpg", "BF006", 35);
 addProduct("Reloj de pulsera", "Reloj de pulsera con correa de cuero", 79.99, "https://ejemplo.com/reloj.jpg", "RL007", 15);
 addProduct("Mochila resistente", "Mochila resistente al agua con múltiples compartimentos", 59.99, "https://ejemplo.com/mochila.jpg", "MC008", 10);
 addProduct("Gafas de sol", "Gafas de sol polarizadas con montura de metal", 34.99, "https://ejemplo.com/gafas.jpg", "GS009", 20);
-addProduct("Pulsera de plata", "Pulsera de plata con diseño elegante", 49.99, "https://ejemplo.com/pulsera.jpg", "PS010", 30);
-addProduct("Zapatillas deportivas", "Zapatillas deportivas para correr", 49.99, "https://ejemplo.com/zapatillas.jpg", "ZP003", 25);
+addProduct("Zapatillas deportivas", "Zapatillas deportivas para correr", 49.99, "https://ejemplo.com/zapatillas.jpg", "ZP003", 25); */
 
+// LLamo a getProducts() luego de haber cargado productos
 // getProducts();
 
+// LLamo a getProductById() luego de haber cargado productos
 // getProductById(4);
 
+// LLamo a updateProduct()
 // updateProduct(2, {"description": "Jeans de corte oversize"});
+
+// Muestro por pantalla para verificar que se haya modificado
 // getProductById(2);
 
-// deleteProduct(7);
+//Llamo a deleteProduct()
+// deleteProduct(5);
+
+// Muestro por pantalla todos los productos para verificar que se haya eliminado
+// getProducts();
 
