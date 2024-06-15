4b76ef4bfae9ad9d5883b11909a62ddd8e6a7ef8
drojas <drojas@cys.com.ar>
2024-06-15 11:39:59 -0300

    desafioEntregable04 - Agrego middleware para login

--- desafioEntregable04/src/routes/index.js
+++ desafioEntregable04/src/routes/index.js
@@ -2,10 +2,11 @@ import { Router } from "express";
 import productsRouters from "./products.routes.js";
 import cartsRouters from "./carts.routes.js";
 import sessionRouters from "./session.routes.js";
+import { isLogin } from "../middlewares/isLogin.middleware.js";
 
 const router = Router();
 
-router.use("/products", productsRouters);
+router.use("/products", isLogin, productsRouters);
 router.use("/carts", cartsRouters);
 router.use("/session", sessionRouters);
 
